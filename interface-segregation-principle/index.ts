9e30fe71210ac8350f40b494599a80ecfca19e30
hw0k <hw0knam@gmail.com>
2020-03-23 22:55:12 +0900

    인터페이스 분리 원칙 예제 완성

--- interface-segregation-principle/index.ts
+++ interface-segregation-principle/index.ts
@@ -1,1 +1,91 @@
 console.log('interface');
+
+namespace isp1 {
+  interface Animal {
+    eat(): void;
+    sleep(): void;
+    cry(): void;
+    fly(): void;
+  }
+  
+  class Bird implements Animal {
+    eat(): void {
+      console.log('새가 음식을 먹었어요!');
+    }
+    sleep(): void {
+      console.log('Zzzzzz....');
+    }
+    cry(): void {
+      console.log('짹짹!!');
+    }
+    fly(): void {
+      console.log('새가 하늘을 날았어요!');
+    }
+  }
+  
+  class Human implements Animal {
+    eat(): void {
+      console.log('아 배부르다');
+    }
+    sleep(): void {
+      console.log('Zzzzzz....');
+    }
+    cry(): void {
+      console.log('ㅠㅠㅠ');
+    }
+    fly(): void {
+      // ????
+    }
+  }
+}
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+namespace isp2 {
+  interface Animal {
+    eat(): void;
+    sleep(): void;
+    cry(): void;
+  }
+
+  interface Flyable {
+    fly(): void;
+  }
+
+  class Bird implements Animal, Flyable {
+    eat(): void {
+      console.log('새가 음식을 먹었어요!');
+    }
+    sleep(): void {
+      console.log('Zzzzzz....');
+    }
+    cry(): void {
+      console.log('짹짹!!');
+    }
+    fly(): void {
+      console.log('새가 하늘을 날았어요!');
+    }
+  }
+
+  class Human implements Animal {
+    eat(): void {
+      console.log('아 배부르다');
+    }
+    sleep(): void {
+      console.log('Zzzzzz....');
+    }
+    cry(): void {
+      console.log('ㅠㅠㅠ');
+    }
+  }
+}
