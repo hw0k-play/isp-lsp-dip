4681a6e602fb62fef141f66962bc31fad5d2a1b1
hw0k <hw0knam@gmail.com>
2020-03-23 22:31:18 +0900

    initial commit

---
+++ index.ts
@@ -0,0 +1,4 @@
+import path from 'path';
+const solidArray = ['dependency-injection-principle', 'interface-segregation-principle', 'liskov-subtitution-principle'];
+
+require(path.join(__dirname, solidArray[1]));
