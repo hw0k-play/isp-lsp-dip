8c977c4538e80979044a8907e294805a49243cf1
Hyunwook Nam <henry@humanscape.co.kr>
2020-03-26 10:49:02 +0900

    리스코프 치환 원칙 오타 수정

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 import path from 'path';
-const solidArray = ['dependency-injection-principle', 'interface-segregation-principle', 'liskov-subtitution-principle'];
+const solidArray = ['dependency-injection-principle', 'interface-segregation-principle', 'liskov-substitution-principle'];
 
-require(path.join(__dirname, solidArray[1]));
+require(path.join(__dirname, solidArray[2]));
