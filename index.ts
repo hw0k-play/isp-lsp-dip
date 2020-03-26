de24220394049b19e84ee85a9fa9a37354f71c7c
Hyunwook Nam <henry@humanscape.co.kr>
2020-03-26 13:22:01 +0900

    DIP 폴더명 수정

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 import path from 'path';
-const solidArray = ['dependency-injection-principle', 'interface-segregation-principle', 'liskov-substitution-principle'];
+const solidArray = ['dependency-inversion-principle', 'interface-segregation-principle', 'liskov-substitution-principle'];
 
 require(path.join(__dirname, solidArray[2]));
